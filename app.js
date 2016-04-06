c814ede807aab9032c5c7bac5666c785b5dffc44
Gildas Cherruel <gildas.cherruel@inin.com>
2016-04-06 14:12:05 +0900

    use express API to create the HTTP server

--- app.js
+++ app.js
@@ -89,26 +89,9 @@ if (app.get('env') === 'development') {
 /**
  * Create HTTP server.
  */
-var server = http.createServer(app);
+app.set('server', app.listen(config.get('port'), function(){
+  console.log("Listening on port %s", config.get('port'));
+}));
 
-server.on('listening', function(){
-  var address = server.address();
-  console.log("Listening on %s", (typeof address === 'string') ? 'pipe ' + address : 'port ' + address.port);
-});
-server.on('error', function(error){
-  if (error.syscal !== 'listen') { throw error; }
-  var bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
-
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default: throw error;
-  }
-});
-server.listen(config.get('port'));
+// Expose app
+exports = module.exports = app;
